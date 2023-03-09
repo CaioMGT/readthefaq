e3a80a63baeecea151e76e94991d9bb5fa665c81
CaioMGT <caio@caiomgt.com>
2023-03-09 19:47:32 -0300

    i got something to work

--- bot.ts
+++ bot.ts
@@ -1,15 +1,13 @@
-import { startBot } from "https://deno.land/x/discordeno@10.0.0/mod.ts";
-startBot({
-    token: "",
-    intents: ["GUILDS", "GUILD_MESSAGES"],
-    eventHandlers: {
-      ready() {
-        console.log("Successfully connected to gateway");
-      },
-      messageCreate(message) {
-        if (message.content === "!ping") {
-          message.reply("Pong using Discordeno!");
-        }
-      },
-    },
-  });
+import { Client, Message, Intents } from 'https://deno.land/x/harmony/mod.ts'
+import "https://deno.land/std@0.178.0/dotenv/load.ts";
+const client = new Client()
+client.on('ready', function(){
+    console.log("Bot started!")
+})
+client.on('messageCreate', function(msg : Message){
+    if (msg.author == client.user) { return}
+    if (msg.content.search("download") && msg.content.search("mod")) {
+        msg.reply("read the faq noob")
+    }   
+})
+client.connect(Deno.env.get("token"), Intents.All)
